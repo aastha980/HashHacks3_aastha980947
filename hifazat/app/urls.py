4d471dbb75a7e7f911b912fd213f1c181568d81e
sa-sa-sa-sa <shipra.agarwal1998@gmail.com>
2018-10-28 01:26:33 +0100

    Initial commit

---
+++ hifazat/app/urls.py
@@ -0,0 +1,9 @@
+from django.conf.urls import url
+from . import views
+app_name='app'
+
+urlpatterns=[
+  url(r'^index$',views.index,name='index'),
+  url(r'^route$',views.route,name='route'),
+  
+]
---
+++ hifazat/app/views.py
@@ -0,0 +1,16 @@
+from django.shortcuts import render
+from django.views.decorators.csrf import csrf_exempt
+# Create your views here.
+def index(request):
+    return render(request,'base.html')
+
+@csrf_exempt
+def route(request):
+    if request.is_ajax():
+        result = request.POST.get("data")
+        x = result.split()
+        distance=x[0]
+        distance=float(distance)
+        if(distance>10):
+            print("Bus derouted")
+    return render(request,'app/route.html')
